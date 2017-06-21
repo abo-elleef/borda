e6e4dd653de4a505c48649675752888a95dcd697
leef <a.aboelllef@gmail.com>
2017-06-21 09:32:36 +0200

    add icons and splash screen and review borda text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AdMob } from '@ionic-native/admob';
 
 import { TabsPage } from '../pages/tabs/tabs';
+interface AdMobType {
+  banner: string,
+  interstitial: string
+};
 
 @Component({
   templateUrl: 'app.html'
@@ -11,12 +16,35 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private admob: AdMob) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      var admobid: AdMobType;
+      if (/(android)/i.test(navigator.userAgent)) {
+        admobid = { // for Android
+          banner: 'ca-app-pub-2772630944180636/3185523871',
+          interstitial: 'ca-app-pub-234234234234324/234234234234'
+        };
+      } else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+        admobid = { // for iOS
+          banner: 'ca-app-pub-234234234234324/234234234234',
+          interstitial: 'ca-app-pub-234234234234324/234234234234'
+        };
+      } else {
+        admobid = { // for Windows Phone
+          banner: 'ca-app-pub-234234234234324/234234234234',
+          interstitial: 'ca-app-pub-234234234234324/234234234234'
+        };
+      }
+
+      this.admob.createBanner({
+        adId: admobid.banner,
+        // isTesting: true,//comment this out before publishing the app
+        autoShow: true
+      })
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,40 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ChapterDetails } from '../pages/chapter-details/chapter-details';
+ import { ChapterDetailsModule } from '../pages/chapter-details/chapter-details.module';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AdMob } from '@ionic-native/admob';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
-    ChapterDetails
+    TabsPage
   ],
   imports: [
     BrowserModule,
+    ChapterDetailsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
     ContactPage,
     HomePage,
     TabsPage,
-    ChapterDetails
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    AdMob,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
